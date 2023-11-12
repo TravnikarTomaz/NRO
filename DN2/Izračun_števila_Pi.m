21ff3dd222084f8f9e82fb3f1a48a4ed181e1b84
Tomaž Travnikar <148692524+TravnikarTomaz@users.noreply.github.com>
2023-11-12 11:23:50 +0100

    Datoteke za domačo nalogo

---
+++ DN2/Izračun_števila_Pi.m
@@ -0,0 +1,19 @@
+(* ::Package:: *)
+
+(*Main.nb*)
+Get["C:\\Users\\tomaz\\OneDrive\\Dokumenti\\Fakulteta za strojni\[SHacek]tvo\\3.letnik 1 semester\\Napredna ra\[CHacek]unalni\[SHacek]ka orodja\\Naloga 2\\MonteCarloPi.m"]
+
+n=100000;(*\[CapitalSHacek]tevilo naklju\[CHacek]nih to\[CHacek]k*)
+
+approxPi=MonteCarloPi[n];
+Print["Pribli\[ZHacek]na vrednost za \[Pi]:",approxPi]
+
+(*Grafi\[CHacek]ni prikaz*)
+
+points=Table[{RandomReal[],RandomReal[]},{i,n}];
+insidePoints=Select[points,#[[1]]^2+#[[2]]^2<=1&];
+
+ListPlot[{points,insidePoints},PlotStyle->{{PointSize[0.01],Red},{PointSize[0.01],Blue}},AxesOrigin->{0,0},PlotLegends->{"Vse to\[CHacek]ke","To\[CHacek]ke v krogu"}]
+
+
+
